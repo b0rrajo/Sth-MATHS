adcc2c767816991a56eca601669469010b674a95
Daniel Borrajo Fdez <119629992+4411daniel@users.noreply.github.com>
2023-09-19 09:18:57 +0200

    Cmath V1.0 ALPHA VERSION

---
+++ Cmath.cpp
@@ -0,0 +1,24 @@
+//Librerías incluidas
+#include<iostream>
+using namespace std;
+
+//Lista de Variables
+int n1;
+int n2;
+int multi;
+
+//Descripcion de variables
+
+
+int main(){
+	cout<<"CMath V1.0"<<endl;
+	cout<<"Programa de operaciones de aritmética"<<endl;
+	cout<<"Para multiplicar, introduzca 2 números"<<endl;
+	cout<<"1º Número";
+	cin>>n1;
+	cout<<"2º nümero";
+	cin>>n2;
+	multi=n1*n2;
+	cout<<"total:"<<multi;
+	return 0;
+}
