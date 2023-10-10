d8bc83cf4560dc20500daa119bb14e2f6409e498
Daniel Borrajo Fdez <119629992+4411daniel@users.noreply.github.com>
2023-10-10 09:10:28 +0200

    Programas chorras xd

---
+++ merepitoitoito.cpp
@@ -0,0 +1,23 @@
+//librerias incluidas
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+//variables incluidas
+int a, n1, n2, n3;
+
+
+//funcion principal
+int main(){
+	cout<<"me repito pito pito..... V1.0"<<endl;
+	cout<<"indique el número de repeticiones maximo que el programa repetira la frase"<<endl;
+	cin>>n1;
+	
+	for(a=n1;a>=1;a--){
+		cout<<"Me repito pito pito pito pito pito pito pito pito pito pito pito pito pito pito pito pito pito pito.................."<<endl;
+	}
+	cout<<"Gracias por perder tu tiempo conmigo xd"<<endl;
+	cout<<"pasa buen día"<<endl;
+
+	return 0;
+}
